49e122e4736b6a5561825675b2f015de0ad96400
Omololu Emmannuel <emmaomoh@gmail.com>
2020-07-15 15:18:43 +0100

    setup project

---
+++ src/pages/login/login.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>This is the Login page</h1>
+  </div>
+</template>
+
+<script src="./login.js"></script>
+
+<style scoped>
+@import url("./login.css");
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  mode: "history",
+  scrollBehavior() {
+    return { x: 0, y: 0 };
+  },
+
+  /*list of all routes */
+  routes: [
+    {
+      path: "/",
+      component: () => import("../pages/login/login.vue"),
+    },
+  ],
+});
+
+export default router;
